9c804e5b60d4bda902502b40d42493931297f945
SAMWEL-IT <samwelmwita062005@gmail.com>
2026-04-25 13:01:18 +0300

    Add functions to open Facebook and Instagram

--- My website.js
+++ My website.js
@@ -1,1 +1,12 @@
+function openFacebook() {
+    window.open("https://facebook.com", "_blank");
+}
 
+function openInstagram() {
+    window.open("https://instagram.com", "_blank");
+}
+
+function sendMessage() {
+    alert("Message sent successfully!");
+    return false;
+}
